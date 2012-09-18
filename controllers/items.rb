c68313cf03f83122088c78da4ca5c84dcd1f8a16
jamiehodge <jamiehodge@me.com>
2012-09-18 14:30:28 +0200

    Add authors, keywords, tags and links to items

--- controllers/items.rb
+++ controllers/items.rb
@@ -62,6 +62,142 @@ class Items < Base
       
         slim :'items/index', locals: { items: items_index }
       end
+      
+      namespace '/authors' do
+        
+        post do
+          author = KU::Media::Person.find_or_create id: params[:author][:id]
+          @item.add_author author unless @item.authors.include? author
+          
+          etag          @item.lock_version
+          last_modified @item.updated_at
+        
+          headers 'Content-Location' => url("/#{@item.id}")
+      
+          slim :'items/show', locals: { item: @item }
+        end
+        
+        namespace '/:id' do
+          
+          before do
+            @author = KU::Media::Person[params[:id]] || not_found
+          end
+          
+          delete do
+            @item.remove_author @author
+            
+            etag          @item.lock_version
+            last_modified @item.updated_at
+        
+            headers 'Content-Location' => url("/#{@item.id}")
+      
+            slim :'items/show', locals: { item: @item }
+          end
+        end
+      end
+      
+      namespace '/keywords' do
+        
+        post do
+          keyword = KU::Media::Keyword.find_or_create name: params[:keyword][:name]
+          @item.add_keyword keyword unless @item.keywords.include? keyword
+          
+          etag          @item.lock_version
+          last_modified @item.updated_at
+        
+          headers 'Content-Location' => url("/#{@item.id}")
+      
+          slim :'items/show', locals: { item: @item }
+        end
+        
+        namespace '/:id' do
+          
+          before do
+            @keyword = KU::Media::Keyword[params[:id]] || not_found
+          end
+          
+          delete do
+            @item.remove_keyword @keyword
+            
+            etag          @item.lock_version
+            last_modified @item.updated_at
+        
+            headers 'Content-Location' => url("/#{@item.id}")
+      
+            slim :'items/show', locals: { item: @item }
+          end
+        end
+      end
+      
+      namespace '/tags' do
+        
+        post do
+          tag = KU::Media::Tag.find_or_create name: params[:tag][:name]
+          @item.add_tag tag unless @item.tags.include? tag
+          
+          etag          @item.lock_version
+          last_modified @item.updated_at
+        
+          headers 'Content-Location' => url("/#{@item.id}")
+      
+          slim :'items/show', locals: { item: @item }
+        end
+        
+        namespace '/:id' do
+          
+          before do
+            @tag = KU::Media::Tag[params[:id]] || not_found
+          end
+          
+          delete do
+            @item.remove_tag @tag
+            
+            etag          @item.lock_version
+            last_modified @item.updated_at
+        
+            headers 'Content-Location' => url("/#{@item.id}")
+      
+            slim :'items/show', locals: { item: @item }
+          end
+        end
+      end
+      
+      namespace '/links' do
+        
+        post do
+          link = KU::Media::Link.find_or_create url: params[:link][:url]
+          @item.add_link link unless @item.links.include? link
+          
+          etag          @item.lock_version
+          last_modified @item.updated_at
+        
+          headers 'Content-Location' => url("/#{@item.id}")
+      
+          slim :'items/show', locals: { item: @item }
+        end
+        
+        namespace '/:id' do
+          
+          before do
+            @link = KU::Media::Link[params[:id]] || not_found
+          end
+          
+          get do
+            200
+          end
+          
+          delete do
+            @item.remove_link @link
+            
+            etag          @item.lock_version
+            last_modified @item.updated_at
+        
+            headers 'Content-Location' => url("/#{@item.id}")
+      
+            slim :'items/show', locals: { item: @item }
+          end
+        end
+      end
     end
   end
   
